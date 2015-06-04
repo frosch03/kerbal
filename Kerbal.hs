a2d9665b8674e010bbf2f5bd77c7f184f684659e
Matthias Brettschneider <frosch03@gmail.com>
2015-06-04 16:56:51 +0200

    complete redesign (old code in comments)

--- Kerbal.hs
+++ Kerbal.hs
@@ -2,6 +2,9 @@
 module Kerbal
 where
 
+-- import Debug.Trace
+import Data.Maybe (fromJust)
+
 type GravConst = Double
 
 var_G :: GravConst
@@ -14,99 +17,145 @@ data Object
       deriving (Eq)
 
 data Celestial
-    = Celestial { r   :: Double
-                , m   :: Double
-                , soi :: Double}
-    deriving (Eq)
+    = Celestial { r      :: Double
+                , m      :: Double
+                , soi    :: Double
+                , system :: System
+                } deriving (Eq)
 
 type DeltaV = Double
 
-type Railed  = (Object, Celestial)
-type Movable = (Object, DeltaV)
+-- type Railed  = (Object, Celestial)
+-- type Movable = (Object, DeltaV)
 
-data Body a where
-    St :: Object -> Celestial -> Body Railed
-    Pl :: Object -> Celestial -> Body Railed
-    Mn :: Object -> Celestial -> Body Railed
-    Sh :: Object -> DeltaV    -> Body Movable
+data Body
+    = Railed  { object :: Object, celestial :: Celestial }
+    | Movable { object :: Object, celestial :: Celestial, deltaV :: DeltaV }
+    deriving (Eq)
 
-object :: Body a -> Object
-object (St o _) = o
-object (Pl o _) = o
-object (Mn o _) = o
-object (Sh o _) = o
-
-isRailed :: Body a -> Bool
-isRailed (St _ _) = True
-isRailed (Pl _ _) = True
-isRailed (Mn _ _) = True
-isRailed (Sh _ _) = False
-
-isMovable :: Body a -> Bool
-isMovable (St _ _) = False
-isMovable (Pl _ _) = False
-isMovable (Mn _ _) = False
-isMovable (Sh _ _) = True
-
-celestial :: Body Railed -> Celestial
-celestial (St _ c) = c
-celestial (Pl _ c) = c
-celestial (Mn _ c) = c
-
-deltaV :: Body Movable -> DeltaV
-deltaV (Sh _ d) = d
+-- data Body where
+--     St :: Object -> Celestial -> Body
+--     Pl :: Object -> Celestial -> Body
+--     Mn :: Object -> Celestial -> Body
+--     Sh :: Object -> DeltaV    -> Body Movable
 
 
-instance Eq (Body Railed) where
-    (St o c) == x = o == object x && c == celestial x
-    (Pl o c) == x = o == object x && c == celestial x
-    (Mn o c) == x = o == object x && c == celestial x
+-- class IsRailed a where
+--     toRailed :: Body -> Body
+               
+-- class IsMovable a where
+--     toMovable :: Body -> Body Movable
+               
+-- instance IsRailed (Railed) where
+--     toRailed x@(St _ _) = x
+--     toRailed x@(Pl _ _) = x
+--     toRailed x@(Mn _ _) = x
 
-instance Eq (Body Movable) where
-    (Sh o d) == x = o == object x && d == deltaV x
+-- instance IsMovable (Movable) where
+--     toMovable x@(Sh _ _) = x
+    
+-- object :: Body -> Object
+-- object (St o _) = o
+-- object (Pl o _) = o
+-- object (Mn o _) = o
+-- object (Sh o _) = o
+
+-- celestial :: (IsRailed a) => Body -> Celestial
+-- celestial (St _ c) = c
+-- celestial (Pl _ c) = c
+-- celestial (Mn _ c) = c
+
+-- deltaV :: (IsMovable a) => Body ->  DeltaV
+-- deltaV (Sh _ d) = d
+
+
+-- instance Eq (Body) where
+--     (St o c) == x = o == object x && c == (celestial x)
+--     (Pl o c) == x = o == object x && c == (celestial x)
+--     (Mn o c) == x = o == object x && c == (celestial x)
+
+-- instance Eq (Body Movable) where
+--     (Sh o d) == x = o == object x && d == (deltaV x)
 
 data Orbit
     = Landed
-    | O { centerBody  :: Body Railed
+    | O { centerBody  :: Body
         , apoapsis    :: Double
         , periapsis   :: Double
         , inclination :: Maybe Double
         , omega_big   :: Maybe Double -- Longitude of Ascending Node
         , omega_small :: Maybe Double -- Argument of Periapsis
         }
-    deriving (Eq)
+   deriving (Eq)
 
-type System a = [(Body a, Orbit)]
+type System = [(Body, Orbit)]
+-- data System a
+--     =  (.-) (Body) (Orbit)
+
 
 type Speed = Double
 
-instance Show (Object)
+instance Show Object
     where
       show = name
 
-instance Show (Body a)
-    where
-      show x@(St _ _) = '[':(show . object $ x) ++ "]"
-      show x@(Pl _ _) = '<':(show . object $ x) ++ ">"
-      show x@(Mn _ _) = '|':(show . object $ x) ++ "|"
-      show x@(Sh _ _) = '{':(show . object $ x) ++ "}"
+-- instance Show (Body)
+--     where
+--       show x@(St _ _) = '[':(show . object $ x) ++ "]"
+--       show x@(Pl _ _) = '<':(show . object $ x) ++ ">"
+--       show x@(Mn _ _) = '|':(show . object $ x) ++ "|"
+--       show x@(Sh _ _) = '{':(show . object $ x) ++ "}"
 
-instance Show (Orbit)
+instance Show Celestial
+    where
+      showsPrec d (Celestial r m soi s)
+          = (showString "r:")
+          . ((show r) ++)
+          . (showString ", ")
+          . (showString "m:")
+          . ((show m) ++)
+          . (showString ", soi:")
+          . (if (soi < (1/0))
+             then ((show soi) ++)
+             else (showString "inf"))
+          . (fun s)
+          where
+            fun []     = (showString "")
+            fun (x:xs) = (showChar '\n') . (showsPrec d x) . (fun xs)
+                           
+
+instance Show Body
+    where
+      showsPrec p (Railed  o c)
+          =  ((name o) ++)
+          . (showString ": ")
+          . ((show c) ++)
+      showsPrec p (Movable o c d)
+          =  ((name o) ++)
+          . (showString ", dV:")
+          . ((show d) ++)
+          . ((show c) ++)
+
+instance Show Orbit
     where
       show Landed = "Landed"
-      show obt =    "Around " ++ (show . centerBody $ obt)
-                 ++ " ( " ++ (show . apoapsis $ obt)
-                 ++ " / " ++ (show . periapsis $ obt)
-                 ++ ")"
+      showsPrec _ obt
+          = (showString "Around ")
+          . (showString $ show . object . centerBody $ obt)
+          . (showString " ( ")
+          . (showString $ show . apoapsis $ obt)
+          . (showString " / ")
+          . (showString $ show . periapsis $ obt)
+          . (showString ")")
 
-
-
-type From   = Body Railed
-type To     = Body Railed
+-- type From   = Body
+-- type To     = Body
 type Radius = Double -- Radius of Orbit at the current Position
-semiMajor :: Orbit -> Double
+semiMajor   :: Orbit -> Double
 semiMajor o
     = 0.5 * ((apoapsis o) + (periapsis o) + (2 * (r . celestial . centerBody $ o)))
+
+      
 
 v :: Orbit -> Radius -> Speed
 v o radius =  sqrt $ mue * ( (2/radius) - (1/(semiMajor o)) ) 
@@ -115,15 +164,15 @@ v o radius =  sqrt $ mue * ( (2/radius) - (1/(semiMajor o)) )
 
 -- get that transition to the (System a) type working
 
-getNextUp :: System a -> From -> Maybe To
+getNextUp :: System -> Body -> Maybe (Body)
 getNextUp s f
     | (soi . celestial $ f) < (1/0)
-    = Just . centerBody . snd . head $ filter ((== f) . fst) s
+    = Just $ centerBody . snd . head $ filter ((== f) . fst) s
 
     | otherwise
     = Nothing
 
-getPathUp :: System a -> From -> [To]
+getPathUp :: System -> Body -> [(Body)]
 getPathUp s f
     = func (Just f)
       where
@@ -131,7 +180,7 @@ getPathUp s f
         func (Just b)  = b : func (getNextUp s b)
                           
 
-getDivid :: System a -> From -> To -> (Int, To)
+getDivid :: System -> Body -> (Body) -> (Int, (Body))
 getDivid s f t
     = ((fst res), (fst . snd $ res))
       where fU  = reverse $ getPathUp s f
@@ -148,12 +197,12 @@ takeUntil f (x:xs)
     | otherwise
     = []
 
-pathBetween :: System a -> From -> To -> [To]
+pathBetween :: System -> Body -> (Body) -> [(Body)]
 pathBetween s f t
     = fU ++ (d:tUR)
       where (fU, d, tUR) = pathBetween' s f t
 
-pathOBetween :: System a -> From -> To -> [(Body Railed, Orbit)]
+pathOBetween :: System -> Body -> (Body) -> [(Body, Orbit)]
 pathOBetween s f t
     = (zip fU fU') ++ (zip tUR tUR')
       where (fU, d, tUR) = pathBetween' s f t
@@ -162,18 +211,18 @@ pathOBetween s f t
             tUR' = map (\x -> snd . head $ filter ((== x) . fst) s) tUR
             d'   = (\x -> snd . head $ filter ((== x) . fst) s) d
 
-pathBetween_ :: System a -> From -> To -> [To]
+pathBetween_ :: System -> Body -> (Body) -> [(Body)]
 pathBetween_ s f t
     = fU ++ tUR
       where (fU, d, tUR) = pathBetween' s f t
 
                            
-pathBetween' :: System a -> From -> To -> ([To], To, [To])
+pathBetween' :: System -> Body -> (Body) -> ([(Body)], (Body), [(Body)])
 pathBetween' s f t
     = (fU, d, (reverse tU))
       where fU   = takeUntil (== d) $ getPathUp s f
             tU   = takeUntil (== d) $ getPathUp s t
             d    = snd $ getDivid s f t 
 
-speeds :: [(Body Railed, Orbit)] -> [Speed]
+speeds :: [(Body, Orbit)] -> [Speed]
 speeds = map (\(_, x) -> v x (semiMajor x))
--- KerbalSystem.hs
+++ KerbalSystem.hs
@@ -3,122 +3,198 @@ where
 
 import Kerbal
 
--- kKerbol = St (BM "Kerbol") (Celestial 261.6e6        0.0         1.7565670e28)
-kKerbol = St (Object "Kerbol") (Celestial 261.6e6 1.7565670e28      (1/0)        )
-kMoho   = Pl (Object "Moho"  ) (Celestial   250e3 2.5263617e21  9646663          )
-kEve    = Pl (Object "Eve"   ) (Celestial   700e3 1.2244127e23 85109365          )
-kGilly  = Mn (Object "Gilly" ) (Celestial    13e3 1.2420512e17   126123.27       )
-kKerbin = Pl (Object "Kerbin") (Celestial   600e3 5.2915793e22 84159286          )
-kMun    = Mn (Object "Mun"   ) (Celestial   200e3 9.7600236e20  2429559.1        )
-kMinmus = Mn (Object "Minmus") (Celestial    60e3 2.6457897e19  2247428.4        )
-kDuna   = Pl (Object "Duna"  ) (Celestial   320e3 4.5154812e21 47921949          )
-kIke    = Mn (Object "Ike"   ) (Celestial   130e3 2.7821949e20  1049598.9        )
-kDres   = Pl (Object "Dres"  ) (Celestial   138e3 3.2191322e20 32832840          )
-kJool   = Pl (Object "Jool"  ) (Celestial     6e6 4.2332635e24        2.4559852e9)
-kLaythe = Mn (Object "Laythe") (Celestial   500e3 2.9397663e22  3723645.8        )
-kVall   = Mn (Object "Vall"  ) (Celestial   300e3 3.1088028e21  2406401.4        )
-kTylo   = Mn (Object "Tylo"  ) (Celestial   600e3 4.2332635e22 10856518          )
-kBop    = Mn (Object "Bop"   ) (Celestial    65e3 3.7261536e19  1221060.9        )
-kPol    = Mn (Object "Pol"   ) (Celestial    44e3 1.0813636e19  1042138.9        )
-kEeloo  = Pl (Object "Eeloo" ) (Celestial   210e3 1.1149358e21        1.1908294e8)
-        
-kSystem :: System Railed
-kSystem
-    = [ (kGilly,   (O { centerBody = kEve
-                      , apoapsis   = (  8.825e6    -  700e3)
-                      , periapsis  = (14.175e6     -  700e3)
-                      , inclination = Just 12
-                      , omega_big   = Just 80
-                      , omega_small = Just 10 }))
-      , (kMun,     (O { centerBody = kKerbin
-                      , apoapsis   = (    12e6     -  600e3)
-                      , periapsis  = (12e6         -  600e3)
-                      , inclination = Nothing
-                      , omega_big   = Nothing
-                      , omega_small = Nothing }))
-      , (kMinmus,  (O { centerBody = kKerbin
-                      , apoapsis   = (    47e6     -  600e3)
-                      , periapsis  = (47e6         -  600e3)
-                      , inclination = Just 6
-                      , omega_big   = Just 78
-                      , omega_small = Just 38 }))
-      , (kIke,     (O { centerBody = kDuna
-                      , apoapsis   = (  3.296e6    -  320e3)
-                      , periapsis  = (3.104e6      -  320e3)
-                      , inclination = Just 0.03
-                      , omega_big   = Nothing
-                      , omega_small = Nothing }))
-      , (kLaythe,  (O { centerBody = kJool
-                      , apoapsis   = ( 27.184e6    -    6e6)
-                      , periapsis  = ( 27.184e6    -    6e6)
-                      , inclination = Nothing
-                      , omega_big   = Nothing
-                      , omega_small = Nothing }))
-      , (kVall,    (O { centerBody = kJool
-                      , apoapsis   = ( 43.152e6    -    6e6)
-                      , periapsis  = ( 43.152e6    -    6e6)
-                      , inclination = Nothing
-                      , omega_big   = Nothing
-                      , omega_small = Nothing }))
-      , (kTylo,    (O { centerBody = kJool
-                      , apoapsis   = ( 68.5e6      -    6e6)
-                      , periapsis  = ( 68.5e6      -    6e6)
-                      , inclination = Just 0.025
-                      , omega_big   = Nothing
-                      , omega_small = Nothing }))
-      , (kBop,     (O { centerBody = kJool
-                      , apoapsis   = (158.6975e6   -    6e6)
-                      , periapsis  = ( 98.3025e6   -    6e6)
-                      , inclination = Just 15
-                      , omega_big   = Just 10
-                      , omega_small = Just 25 }))
-      , (kPol,     (O { centerBody = kJool
-                      , apoapsis   = (210.624206e6 -    6e6)
-                      , periapsis  = (149.155794e6 -    6e6)
-                      , inclination = Just 4.25
-                      , omega_big   = Just 2
-                      , omega_small = Just 15 }))
-      , (kMoho,    (O { centerBody = kKerbol
-                      , apoapsis   = (  6315765980 -261.6e6)
-                      , periapsis  = ( 4210510628  -261.6e6)
-                      , inclination = Just 7
-                      , omega_big   = Just 70
-                      , omega_small = Just 15 }))
-      , (kEve,     (O { centerBody = kKerbol
-                      , apoapsis   = (  9931011387 -261.6e6)
-                      , periapsis  = ( 9734357701  -261.6e6)
-                      , inclination = Just 2.1
-                      , omega_big   = Just 15
-                      , omega_small = Nothing }))
-      , (kKerbin,  (O { centerBody = kKerbol
-                      , apoapsis   = ( 13599840256 -261.6e6)
-                      , periapsis  = (13599840256  -261.6e6)
-                      , inclination = Nothing
-                      , omega_big   = Nothing
-                      , omega_small = Nothing }))
-      , (kDuna,    (O { centerBody = kKerbol
-                      , apoapsis   = ( 21783189163 -261.6e6)
-                      , periapsis  = (19669121365  -261.6e6)
-                      , inclination = Just 0.06
-                      , omega_big   = Just 135.5
-                      , omega_small = Nothing }))
-      , (kDres,    (O { centerBody = kKerbol
-                      , apoapsis   = ( 46761053522 -261.6e6)
-                      , periapsis  = (34917642884  -261.6e6)
-                      , inclination = Just 5
-                      , omega_big   = Just 280
-                      , omega_small = Just 90 }))
-      , (kJool,    (O { centerBody = kKerbol
-                      , apoapsis   = ( 72212238387 -261.6e6)
-                      , periapsis  = (65334882253  -261.6e6)
-                      , inclination = Just 1.302
-                      , omega_big   = Just 52
-                      , omega_small = Nothing }))
-      , (kEeloo,   (O { centerBody = kKerbol
-                      , apoapsis   = (113549713200 -261.6e6)
-                      , periapsis  = (66687926800  -261.6e6)
-                      , inclination = Just 6.15
-                      , omega_big   = Just 50
-                      , omega_small = Just 260 }))
-      ]
+kMoho
+    = Railed (Object "Moho"  )
+      (Celestial   250e3 2.5263617e21  9646663 [])
+kGilly
+    = Railed (Object "Gilly" )
+      (Celestial 13e3 1.2420512e17 126123.27 []) 
+kEve
+    = Railed (Object "Eve")
+      (Celestial 700e3 1.2244127e23 85109365
+                     [ (kGilly, kGillyOrbit) ]) 
+kMun
+    = Railed (Object "Mun" )
+      (Celestial 200e3 9.7600236e20 2429559.1 [])
+kMinmus
+    = Railed (Object "Minmus")
+      (Celestial 60e3 2.6457897e19 2247428.4 [])
+kKerbin
+    = Railed (Object "Kerbin")
+      (Celestial 600e3 5.2915793e22 84159286
+                     [ (kMun, kMunOrbit)
+                     , (kMinmus, kMinmusOrbit) ])
+kIke
+    = Railed (Object "Ike" )
+      (Celestial 130e3 2.7821949e20 1049598.9 [])
+kDres
+    = Railed (Object "Dres" )
+      (Celestial 138e3 3.2191322e20 32832840 [])
+kDuna
+    = Railed (Object "Duna" )
+      (Celestial 320e3 4.5154812e21 47921949
+                     [ (kIke, kIkeOrbit)
+                     , (kDres, kDresOrbit) ])
+kLaythe
+    = Railed (Object "Laythe")
+      (Celestial 500e3 2.9397663e22 3723645.8 [])
+kVall
+    = Railed (Object "Vall" )
+      (Celestial 300e3 3.1088028e21 2406401.4 [])
+kTylo
+    = Railed (Object "Tylo" )
+      (Celestial 600e3 4.2332635e22 10856518 [])
+kBop
+    = Railed (Object "Bop" )
+      (Celestial 65e3 3.7261536e19 1221060.9 [])
+kPol
+    = Railed (Object "Pol" )
+      (Celestial 44e3 1.0813636e19 1042138.9 [])
+kJool
+    = Railed (Object "Jool" )
+      (Celestial 6e6 4.2332635e24 2.4559852e9
+                     [ (kLaythe, kLaytheOrbit)
+                     , (kVall, kVallOrbit)
+                     , (kTylo, kTyloOrbit)
+                     , (kBop, kBopOrbit)
+                     , (kPol, kPolOrbit) ])
+kEeloo
+    = Railed (Object "Eeloo" )
+      (Celestial 210e3 1.1149358e21 1.1908294e8 [])
+kKerbol
+    = Railed (Object "Kerbol")
+      (Celestial 261.6e6 1.7565670e28 (1/0)
+                     [ (kMoho, kMohoOrbit)
+                     , (kEve, kEveOrbit)
+                     , (kKerbin, kKerbinOrbit)
+                     , (kDuna, kDunaOrbit)
+                     , (kJool, kJoolOrbit)
+                     , (kEeloo, kEelooOrbit) ])
 
+kGillyOrbit
+    = O { centerBody = kEve
+        , apoapsis = ( 8.825e6 - 700e3)
+        , periapsis = (14.175e6 - 700e3)
+        , inclination = Just 12
+        , omega_big   = Just 80
+        , omega_small = Just 10 }
+              
+kMunOrbit
+    = O { centerBody = kKerbin
+        , apoapsis = ( 12e6 - 600e3)
+        , periapsis = (12e6 - 600e3)
+        , inclination = Nothing
+        , omega_big   = Nothing
+        , omega_small = Nothing }
+            
+kMinmusOrbit
+    = O { centerBody = kKerbin
+        , apoapsis = ( 47e6 - 600e3)
+        , periapsis = (47e6 - 600e3)
+        , inclination = Just 6
+        , omega_big   = Just 78
+        , omega_small = Just 38 }
+               
+kIkeOrbit
+    = O { centerBody = kDuna
+        , apoapsis = ( 3.296e6 - 320e3)
+        , periapsis = (3.104e6 - 320e3)
+        , inclination = Just 0.03
+        , omega_big   = Nothing
+        , omega_small = Nothing }
+            
+kLaytheOrbit
+    = O { centerBody = kJool
+        , apoapsis = ( 27.184e6 - 6e6)
+        , periapsis = ( 27.184e6 - 6e6)
+        , inclination = Nothing
+        , omega_big   = Nothing
+        , omega_small = Nothing }
+               
+kVallOrbit
+    = O { centerBody = kJool
+        , apoapsis = ( 43.152e6 - 6e6)
+        , periapsis = ( 43.152e6 - 6e6)
+        , inclination = Nothing
+        , omega_big   = Nothing
+        , omega_small = Nothing }
+             
+kTyloOrbit
+    = O { centerBody = kJool
+        , apoapsis = ( 68.5e6 - 6e6)
+        , periapsis = ( 68.5e6 - 6e6)
+        , inclination = Just 0.025
+        , omega_big   = Nothing
+        , omega_small = Nothing }
+             
+kBopOrbit
+    = O { centerBody = kJool
+        , apoapsis = (158.6975e6 - 6e6)
+        , periapsis = ( 98.3025e6 - 6e6)
+        , inclination = Just 15
+        , omega_big   = Just 10
+        , omega_small = Just 25 }
+            
+kPolOrbit
+    = O { centerBody = kJool
+        , apoapsis = (210.624206e6 - 6e6)
+        , periapsis = (149.155794e6 - 6e6)
+        , inclination = Just 4.25
+        , omega_big   = Just 2
+        , omega_small = Just 15 }
+            
+kMohoOrbit
+    = O { centerBody = kKerbol
+        , apoapsis = ( 6315765980 -261.6e6)
+        , periapsis = ( 4210510628 -261.6e6)
+        , inclination = Just 7
+        , omega_big   = Just 70
+        , omega_small = Just 15 }
+             
+kEveOrbit
+    = O { centerBody = kKerbol
+        , apoapsis = ( 9931011387 -261.6e6)
+        , periapsis = ( 9734357701 -261.6e6)
+        , inclination = Just 2.1
+        , omega_big   = Just 15
+        , omega_small = Nothing }
+            
+kKerbinOrbit
+    = O { centerBody = kKerbol
+        , apoapsis = ( 13599840256 -261.6e6)
+        , periapsis = (13599840256 -261.6e6)
+        , inclination = Nothing
+        , omega_big   = Nothing
+        , omega_small = Nothing }
+               
+kDunaOrbit
+    = O { centerBody = kKerbol
+        , apoapsis = ( 21783189163 -261.6e6)
+        , periapsis = (19669121365 -261.6e6)
+        , inclination = Just 0.06
+        , omega_big   = Just 135.5
+        , omega_small = Nothing }
+             
+kDresOrbit
+    = O { centerBody = kKerbol
+        , apoapsis = ( 46761053522 -261.6e6)
+        , periapsis = (34917642884 -261.6e6)
+        , inclination = Just 5
+        , omega_big   = Just 280
+        , omega_small = Just 90 }
+             
+kJoolOrbit
+    = O { centerBody = kKerbol
+        , apoapsis = ( 72212238387 -261.6e6)
+        , periapsis = (65334882253 -261.6e6)
+        , inclination = Just 1.302
+        , omega_big   = Just 52
+        , omega_small = Nothing }
+             
+kEelooOrbit
+    = O { centerBody = kKerbol
+        , apoapsis = (113549713200 -261.6e6)
+        , periapsis = (66687926800 -261.6e6)
+        , inclination = Just 6.15
+        , omega_big   = Just 50
+        , omega_small = Just 260 }
